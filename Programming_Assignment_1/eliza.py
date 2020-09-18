60dc18e187908732d071973a2cdb0fae8d6f5473
anughshrestha <shrestha.anugh@gmail.com>
2020-09-18 00:28:21 -0400

    comments

--- Programming_Assignment_1/eliza.py
+++ Programming_Assignment_1/eliza.py
@@ -2,6 +2,11 @@ import re
 import random
 from nltk.tokenize import word_tokenize
 
+##############################################
+#pronouns is a dictionary that is used to transform the pronouns from first-person to second-person
+#When the program receives the user's input, the code will look the pronoun that is listed in the key of the dictionary
+#When the pronoun is found on the key, that particular pronoun will be replace with the pronoun in the corresponding value
+##############################################
 pronouns = {
 	"you": "me",
 	"me": "you",
@@ -12,6 +17,18 @@ pronouns = {
 	"i" : "you"
 }
 
+##############################################
+#possible_response is a dictionary that contain keys and list values.
+#The keys of the dictionary represents that possible patterns of words and sentences in the input that the user may enter
+#The list values represents the list of responses to the matching pattern of words and sentences in the input of the user
+#When the pattern of words or responses matches a key, it will randomly pull a response from the corresponding list value
+#For example: if the user's input has the word happy, the program will randomly select either Why are you happy?, 
+#What made you happy?, or Oh, do you have an exciting news?
+
+#In the response by the chat bot, {0} gets subsituted with set of word(s) that included after the word of the match pattern
+#For example: if the user enters 'it is raining' (which matches the pattern [Ii]t is (.*), the bot might reply back with response that states 
+#'Why is it raining?'
+##############################################
 possible_responses = {
 	r'[Hh](ello|i).*' : ['Hey there!', 'Hello', 'Hi', 'Howdy'],
 	r'\b[Ww]hat is your name?\b' : ['My name is Eliza', 'I am Eliza', 'Eliza'],
@@ -63,55 +80,99 @@ possible_responses = {
 	r'(.*)' : ["Go on!","Tell me more"]
 }
 
+##############################################
+# greeting() method
+# This method greets the user with a greeting message and asks the user for his/her name
+# This method also kicks off the conseling session by asking how they are feeling today
+##############################################
 def greeting():
-	correct_name = False
+	#correct_name is a boolean variable that will be used to determine whether the name of the user matches the pattern.
+	#As described later in the code, the name of the user must contains alphabetical values
+	correct_name = False 
 	name = ""
-	while not correct_name:
+
+	#The loop contains the methods to greet and receive input from the user. This loop will continue asking for the user
+	while not correct_name:   
+		#There is a nested while loop which is designed to keep asking user for their name when they provide an empty response
+		#The name variable stores the name of the user that is provided by the user themselves
 		while name == "":
 			greeting_message = "\n***************************************************\n"
 			greeting_message += "Hello! Welcome to student psychology services\n"
 			greeting_message += "Please feel free to express yourself\n"
 			greeting_message += "Please enter 'quit' if you wish to discontinue at any point\n"
 			greeting_message += "***************************************************\n"
-			print(greeting_message)
-			name = input("Hi, My name is Eliza. What is your name?\n>>")
-			name = str(name.strip())
+			print(greeting_message) #prints the greeting_message that is declared above
+			name = input("Hi, My name is Eliza. What is your name?\n>>") #Throughout the program input() methods will be used to receive user's input
+			name = str(name.strip()) 
 			
-			quit_pattern = r'[Qq]uit;?'
-			quit = re.match(quit_pattern,name)
+			#Checks whether the user enters quit, Quit, quit;, or Quit; The program exits when the user's response matches the word
+			quit_pattern = r'[Qq]uit;?'  
+			quit = re.match(quit_pattern,name) 
 
-			if name == "":
+			#if else statement
+			#Checks if user entered a blank input for their name. 
+			#If the input is blank then it will print out the message stating that the user did not provide their name
+			if name == "": 
 				print("Please give me your name! :)")
 				continue
+
+			#if the user's input is not empty then it checks whether or not the user entered the word quit
+			#if the user did not enter the word quit then it will check the input for validation through greeting_validation method
 			elif not quit:
-				greeting_name = greeting_validation(name, check_y=True)
+				#greeting_validation method checks whether the user's input matches the expected pattern for name
+				#the method returns incorrect if it does not matches the pattern
+				greeting_name = greeting_validation(name, check_y=True) 
 				if greeting_name == "Incorrect":
 					correct_name = False
 					print("Lets try that again!")
 				else:
 					correct_name = True
+
+			#if the user enters the word quit then it calls the bye() method which prints a goodbye message to the user
+			#and exits out from the program
 			else:
 				
 				bye()
 				exit()
+
+	#Then the conseling session is kicked off by asking the user how they are feeling.
+	#Keeps asking user the same question about feelings when they enter an empty input
 	message = ""
 	while message == "":
-		message = input(f"Hello {greeting_name}, How are you feeling today?\n>>")
+		#The question concatenates the user's name that was received from the input above
+		message = input(f"Hello {greeting_name}, How are you feeling today?\n>>") 
 		if message == "":
 			print("You have to tell me how you feel! :)")
-	return message
+	return message #returns the input from the user
 
+##############################################
+#greeting_validation()
+#input: name (string), check_y (boolean)
+#Validates if the name of the user is provided in proper pattern
+##############################################
 def greeting_validation(name, check_y):
 	
+	#the input of the user is split by spaces
 	name_count = name.split()
 
+	#if else statement
+	#Counts if the name input has more than one word
+	#if it has more than one word then it must match the pattern 'My name is ' followed by user's name.
+	#It checks if the name is in alphabetical format
+	#if it does not match the pattern then it will return the word 'Incorrect' 
+	#if it does not match the pattern then it will keep asking user for the input in the greeting() method 
+	#if it matches the pattern then it will return just the user's name
 	if (len(name_count) > 1):
 		pattern = r'\b[Mm]y name is\b ([A-z].+)'
 		x = re.match(pattern,name)
 		if x:
-			return str(x[1])
+			return str(x[1]) 
 		else:
 			return "Incorrect"
+	#if the count if not greater than one then it will check if the input is in alphabetical format
+	#Checks if the name is in alphabetical format
+	#returns incorrect if it does not matches the pattern
+	#the program has the option if this pattern needs to be checked. Hence, check_y parameter determines if this pattern needs to be checked
 	else:
 		if check_y:
 			
@@ -122,6 +183,13 @@ def greeting_validation(name, check_y):
 			else:
 				return "Incorrect"
 
+
+##############################################
+#replace_punctuation()
+#parameters: user_response
+#replaces punctuations in the response provided by the user
+#For example: replaces I'm with I am
+##############################################
 def replace_punctuation(user_response):
 
 	had_pattern = r'[Ii]\'d an?'
@@ -134,65 +202,113 @@ def replace_punctuation(user_response):
 	user_response = re.sub(r'[Ii]\'ve', "I have", user_response)
 	user_response = re.sub(r"[Ii]t\'s", "It is", user_response)
 	user_response = re.sub(r'[Ll]et\'s', "Let us", user_response)
-	print(user_response)
+	#print(user_response)
 
 	return user_response
 
-def generate_response(user_response,last_response):
+##############################################
+#generate_response()
+#parameters: user_response (string)
+#This method checks if the user's input matches the pattern that are listed in the possible_response dictionary
+##############################################
+def generate_response(user_response):
 
-	valid_status = check_validity(user_response)
+	#check_name method checks if the user enters their name again
+	#the detailed description of the method is provided later in the code
+	#if the user didn't not enter their name again then it returns the word 'valid'
+	#if the user did enter their name again then it returns the message 'Hello <user_name>, How are you feeling today?'
+	#the returned message is stored in valid_status variable
+	valid_status = check_name(user_response)
+
+	#calls the replace_punctuation method to replace punctuation (description above)
 	user_response = replace_punctuation(user_response)
 	
+	#if valid_status is equal to valid then it loops through the possible_response dictionary to check if the user's input matches the 
+	#patterns in the dictionary
 	if valid_status == "valid":
 
+		#for loop is used to loop through possible_response dictionary
+		#pattern represents the key where the possible word/sentence patterns are stored
+		#reponse represents the list of reponses for the matching corresponding pattern
 		for pattern, reponse in possible_responses.items():
-			match = re.match(pattern, user_response)
+			match = re.match(pattern, user_response) #match method from re module to check whether the user's reponse matches any patterns
+			#if there is a match, a random response is choosen from the list
 			if match:
-				bot_response = random.choice(reponse)
+				bot_response = random.choice(reponse) 
 				
+				#when there is a match, it gets the words after the matching pattern is located in the sentence 
+				#and sustitues it to the cooresponding reponse that has the symbol {0} 
 				s = ""
 				for x in match.groups():
 					s = s + " " + x
 
+				#evaluate_pronoun method is called to replace the first-person pronoun with second-person pronoun
 				reponse_message = bot_response.format(*[evaluate_pronoun(s)])
 				return reponse_message
+	#if the valid_status is not equal to valid then that means the user entered their name again.
+	#it will ask the user how they are feeling again.
 	else:
 		
 		reponse_message = valid_status 
 		return reponse_message
 
-
+##############################################
+#evaluate_pronoun()
+#It converts all the letters to lowercase and splits the words in the sentence by using word_tokenize method
+##############################################
 def evaluate_pronoun(sentence):
 
 	sentence = sentence.lower()
 	tokens = word_tokenize(sentence)
 
+	#Loops through all the words and replaces the first-person pronoun with second-person pronoun
+	#i is the key and token is the value
 	for i, token in enumerate(tokens):
 		if token in pronouns:
 			tokens[i] = pronouns[token]
 	
 	return ' '.join(tokens)
 
-def check_validity(message):
+##############################################
+#check_name() method
+#parameter input
+#Checks if the the user entered their name again
+##############################################
+def check_name(message):
 	valid_message = "valid"
 	message_1 = message.strip()
-	greeting_check = greeting_validation(message, check_y=False)
+	#calls the greeting_validation method with check_y parameter set to false
+	#as stated above greeting_validation method returns word 'Incorrect' if it is does not match the pattern
 	
+	greeting_check = greeting_validation(message, check_y=False)
+
+	#if it does not return 'Incorrect' in this case then it means that the user's input does not represent user's name
+	#if it does return 'Incorrect' then the user did input their name
 	if greeting_check != None and greeting_check != "Incorrect":
 		
 		valid_message = f"Hello {message}, How are you feeling today?"
 		return valid_message
 	return valid_message
 
+##############################################
+#bye()
+#output's goodbye message for user
+#picks a random word from the possible_bye_messages
+##############################################
 def bye():
 
 	possible_bye_messages = ["It was pleasure talking to you.", "Bye have a nice day.", "See you soon, take care."]
 	bye_message = random.choice(possible_bye_messages)
 	print(bye_message)
 
+##############################################
+#main() method
+#main operator of the program
+##############################################
+
 def main():
 
-	greeting_message = greeting()
+	greeting_message = greeting() #calls greeting method
 	last_response = greeting_message
 	message = greeting_message
 
@@ -200,10 +316,11 @@ def main():
 	last_question = None
 
 	quit_pattern = r'[Qq]uit;?'
-	quit = re.match(quit_pattern,message)
+	quit = re.match(quit_pattern,message) #checks if the user's input matches the word quit and it stops the program
 
-	
+	#it will keep asking user questions until the user enter the word quit
 	while not quit:
+		#checks if the user entered a blank input
 		if message == "":
 			print("You have to tell me something! :)")
 			message = input(f"{response_question}\n>>")
@@ -213,9 +330,11 @@ def main():
 			if matching_resonse:
 				response_question = last_question
 			else:
-				response_question = generate_response(str(message),last_response)
+				response_question = generate_response(str(message))#calls the generate_response method declared above
 
-			message = input(f"{response_question}\n>>")
+			message = input(f"{response_question}\n>>") #input function is used to get user's input and stored in message variable
+
+			#checks if the user is repeating themselves
 			if last_response == message :
 				print("You can't repeat yourself! :)\nPlease response to the response below....")
 				last_question = response_question
@@ -223,8 +342,9 @@ def main():
 			else:
 				matching_resonse = False
 
-			last_response = message
-		quit = re.match(quit_pattern,message)
+			#stores every user's input into last_response variable and it is compared with subsequent response to see if they are the same
+			last_response = message  
+		quit = re.match(quit_pattern,message) #checks if the user's input matches the word quit and it stops the program
 	bye()
 
 main()
